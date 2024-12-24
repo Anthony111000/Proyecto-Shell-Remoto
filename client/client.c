b312944d102ee901ef6c202f4df64e381fcdfe93
MenMonGit <meneses.moncada@correounivalle.edu.co>
2024-12-24 11:57:31 -0500

    comunicacion asertiva entre cliente y servidor mediante el socket, correccion de errores

--- client/client.c
+++ client/client.c
@@ -1,42 +1,44 @@
-#include <netinet/in.h> //structure for storing address information 
+#include <netinet/in.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <sys/socket.h> //for socket APIs 
+#include <string.h> 
+#include <sys/socket.h> 
 #include <sys/types.h> 
-#include <string.h>
-int main(int argc, char const* argv[]) 
-{ 
 
+int main(int argc, char const* argv[]) {
+    char strData[326];
+    int sockD = socket(AF_INET, SOCK_STREAM, 0); 
 
-	char strData[326];
-	int sockD = socket(AF_INET, SOCK_STREAM, 0); 
+    struct sockaddr_in servAddr; 
 
-	struct sockaddr_in servAddr; 
+    servAddr.sin_family = AF_INET; 
+    servAddr.sin_port = htons(9001); 
+    servAddr.sin_addr.s_addr = INADDR_ANY; 
 
-	servAddr.sin_family = AF_INET; 
-	servAddr.sin_port 
-		= htons(9001); // use some unused port number 
-	servAddr.sin_addr.s_addr = INADDR_ANY; 
+    int connectStatus = connect(sockD, (struct sockaddr*)&servAddr, sizeof(servAddr)); 
 
-	int connectStatus 
-		= connect(sockD, (struct sockaddr*)&servAddr, 
-				sizeof(servAddr)); 
+    if (connectStatus == -1) { 
+        printf("Error al conectar con el servidor...\n"); 
+        return -1;
+    } 
 
-	if (connectStatus == -1) { 
-		printf("Error...\n"); 
-	} 
+    printf("Conectado al servidor.\nEscribe 'leave' para salir.\n");
 
-	else {
-		do{//sending message to the server and recieving
-			send(sockD,strData,sizeof(strData),0);
-			fgets(strData,sizeof(strData),stdin);
-			recv(sockD, strData, sizeof(strData), 0); 
-			strData[strcspn(strData,"\n")]='\0';
-			printf("Message: %s\n", strData);
-		}while(strcmp(strData,"leave")!=0); 
-		; 
-	} 
+    do {
+        printf("Introduce un mensaje: ");
+        fgets(strData, sizeof(strData), stdin);
+        strData[strcspn(strData, "\n")] = '\0'; // Eliminar salto de línea
 
+        send(sockD, strData, strlen(strData) + 1, 0); // Enviar mensaje
 
-	return 0; 
+        if (strcmp(strData, "leave") == 0) {
+            break; // Salir del ciclo si el mensaje es "leave"
+        }
+
+        recv(sockD, strData, sizeof(strData), 0); // Recibir respuesta
+        printf("Servidor: %s\n", strData);
+    } while (1);
+
+    close(sockD); // Cerrar socket
+    return 0;
 }
--- server/server.c
+++ server/server.c
@@ -1,49 +1,51 @@
-#include <netinet/in.h> //structure for storing address information 
+#include <netinet/in.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <sys/socket.h> //for socket APIs 
+#include <string.h> 
+#include <sys/socket.h> 
 #include <sys/types.h> 
 
-int main(int argc, char const* argv[]) 
-{ 
+int main(int argc, char const* argv[]) {
+    int servSockD = socket(AF_INET, SOCK_STREAM, 0); 
+    char serMsg[255]; 
 
-	// create server socket similar to what was done in 
-	// client program 
-	int servSockD = socket(AF_INET, SOCK_STREAM, 0); 
+    struct sockaddr_in servAddr; 
+    servAddr.sin_family = AF_INET; 
+    servAddr.sin_port = htons(9001); 
+    servAddr.sin_addr.s_addr = INADDR_ANY; 
 
-	// string store data to send to client 
-	char serMsg[255] = "Message from the server to the "
-					"client \'Hello Client\' "; 
+    bind(servSockD, (struct sockaddr*)&servAddr, sizeof(servAddr)); 
+    listen(servSockD, 1); 
 
-	// define server address 
-	struct sockaddr_in servAddr; 
+    printf("Esperando conexiones...\n");
 
-	servAddr.sin_family = AF_INET; 
-	servAddr.sin_port = htons(9001); 
-	servAddr.sin_addr.s_addr = INADDR_ANY; 
+    int clientSocket = accept(servSockD, NULL, NULL);
+    if (clientSocket == -1) {
+        printf("Error al aceptar la conexión.\n");
+        return -1;
+    }
 
-	// bind socket to the specified IP and port 
-	bind(servSockD, (struct sockaddr*)&servAddr, 
-		sizeof(servAddr)); 
+    printf("Cliente conectado.\n");
 
-	//adding multiple execute
-	
-		// listen for connections 
-	listen(servSockD, 1); 
+    do {
+        memset(serMsg, 0, sizeof(serMsg)); // Limpiar buffer
+        int bytesReceived = recv(clientSocket, serMsg, sizeof(serMsg), 0);
 
-	// integer to hold client socket. 
-	int clientSocket = accept(servSockD, NULL, NULL);
-	do{
-	//recieve message from the client socket
-	printf("%ld",recv(servSockD,serMsg,sizeof(serMsg),0)); 
+        if (bytesReceived > 0) {
+            printf("Cliente: %s\n", serMsg);
 
-	// send's messages to client socket 
-	send(clientSocket, serMsg, sizeof(serMsg), 0); 
+            if (strcmp(serMsg, "leave") == 0) {
+                printf("El cliente se ha desconectado.\n");
+                break;
+            }
 
-	} while(1==1);
-	//
+            // Enviar respuesta
+            char respuesta[] = "Mensaje recibido";
+            send(clientSocket, respuesta, strlen(respuesta) + 1, 0);
+        }
+    } while (1);
 
-	
-	return 0; 
+    close(clientSocket); 
+    close(servSockD); 
+    return 0;
 }
-
