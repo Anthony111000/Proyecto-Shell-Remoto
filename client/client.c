93abf6c1bf3b97aca8cc581681fef51dddb5e31c
MenMonGit <meneses.moncada@correounivalle.edu.co>
2024-12-27 18:43:08 -0500

    Conexion entre ambos sin errores, permitiendo ejecutar ahora mensajes que digita el usuario y mostrados en servidor, falta hacer que sea ejecutado al crear un proceso y programar la recepcion en el servidor de igual manera
    2

--- client/client.c
+++ client/client.c
@@ -4,13 +4,33 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <sys/types.h> 
+#include <pthread.h>
+
+
+char senter(char *strData,int sockD){
+do {//ciclo que repite  ejecucion hasta comando leave
+    printf("Introduce un mensaje: ");
+    fgets(strData, sizeof(strData), stdin);
+    strData[strcspn(strData, "\n")] = '\0'; // Eliminar salto delínea
+    send(sockD, strData, strlen(strData) + 1, 0); // Enviar mensaje
+    if (strcmp(strData, "leave") == 0) {
+	    break; // Salir del ciclo si el mensaje es "leave"
+        }
+    recv(sockD, strData, sizeof(strData), 0); // Recibir respuesta
+    printf("Servidor: %s\n", strData);
+    } while (1);
+}
+
+
+
+
+
 
 int main(int argc, char const* argv[]) {
     char strData[326];
     int sockD = socket(AF_INET, SOCK_STREAM, 0); 
 
     struct sockaddr_in servAddr; 
-
     servAddr.sin_family = AF_INET; 
     servAddr.sin_port = htons(9001); 
     servAddr.sin_addr.s_addr = INADDR_ANY; 
@@ -23,21 +43,8 @@ int main(int argc, char const* argv[]) {
     } 
 
     printf("Conectado al servidor.\nEscribe 'leave' para salir.\n");
-
-    do {
-        printf("Introduce un mensaje: ");
-        fgets(strData, sizeof(strData), stdin);
-        strData[strcspn(strData, "\n")] = '\0'; // Eliminar salto de línea
-
-        send(sockD, strData, strlen(strData) + 1, 0); // Enviar mensaje
-
-        if (strcmp(strData, "leave") == 0) {
-            break; // Salir del ciclo si el mensaje es "leave"
-        }
-
-        recv(sockD, strData, sizeof(strData), 0); // Recibir respuesta
-        printf("Servidor: %s\n", strData);
-    } while (1);
+    senter(strData,sockD);
+    //ejecuciones
 
     close(sockD); // Cerrar socket
     return 0;
